003e5510bf6a2eafaebb446cc95a3fc3f3b87db8
Edward Chao <edward.chao@flyribbit.com>
2025-04-03 17:14:34 -0400

    add params for net

--- src/novatel_oem7_driver/launch/novatel_launch.py
+++ src/novatel_oem7_driver/launch/novatel_launch.py
@@ -10,13 +10,16 @@ def generate_launch_description():
 
     launch_file_path = PathJoinSubstitution([FindPackageShare(pkg_name),
                                              "launch",
-                                             "oem7_port.launch.py"])
+                                             "oem7_net.launch.py"])
 
     launch_source = PythonLaunchDescriptionSource([launch_file_path])
 
     arguments = {
         "oem7_port_name": "/dev/ttyS1",
         "oem7_port_baud": "57600",
+        "oem7_if": "Oem7ReceiverTcp"
+        "oem7_ip_addr": "10.1.0.212"
+        "oem7_port": "3001"
     }.items()
 
     return LaunchDescription([
